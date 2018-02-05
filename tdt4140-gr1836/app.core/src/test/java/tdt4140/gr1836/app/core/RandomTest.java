5ad63a479dabf357c98232dad0f35f5616623086
skotn <31900309+skotn@users.noreply.github.com>
2018-02-05 17:16:59 +0100

    Lagt til test

---
+++ tdt4140-gr1836/app.core/src/test/java/tdt4140/gr1836/app/core/RandomTest.java
@@ -0,0 +1,14 @@
+package tdt4140.gr1836.app.core;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class RandomTest {
+	@Test
+	public void test() {
+		assertEquals(1,0);
+		
+	}
+
+}
