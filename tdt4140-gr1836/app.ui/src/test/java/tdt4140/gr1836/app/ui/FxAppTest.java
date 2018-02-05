6520a8dc24f149f504a1498ead67a2541b834512
skotn <31900309+skotn@users.noreply.github.com>
2018-02-05 16:57:46 +0100

    CI for 36

--- tdt4140-gr1836/app.ui/src/test/java/tdt4140/gr1836/app/ui/FxAppTest.java
+++ tdt4140-gr1836/app.ui/src/test/java/tdt4140/gr1836/app/ui/FxAppTest.java
@@ -1,5 +1,6 @@
 package tdt4140.gr1836.app.ui;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 
@@ -9,7 +10,13 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class FxAppTest extends ApplicationTest {
-
+	
+	@BeforeClass
+	public static void headless() {
+		if (Boolean.valueOf(System.getProperty("gitlab-ci", "false")))
+		GitlabCISupport.headless();
+	}
+	
 	@Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FxApp.fxml"));
