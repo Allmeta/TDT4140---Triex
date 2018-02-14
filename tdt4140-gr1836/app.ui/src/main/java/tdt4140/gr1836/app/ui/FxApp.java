574b6484af4cc07b2dfaa02977b26948ccbd82d3
unknown <erling_sm@hotmail.com>
2018-02-14 11:06:08 +0100

    login screen

--- tdt4140-gr1836/app.ui/src/main/java/tdt4140/gr1836/app/ui/FxApp.java
+++ tdt4140-gr1836/app.ui/src/main/java/tdt4140/gr1836/app/ui/FxApp.java
@@ -1,24 +1,107 @@
 package tdt4140.gr1836.app.ui;
 
+import java.io.FileInputStream;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.Group;
 
 public class FxApp extends Application {
+	
+	Scene scene1,scene2;
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FxApp.fxml"));
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+    @SuppressWarnings("restriction")
+	public void start(Stage primaryStage) throws Exception{
+    	
+    	//FXML Loader
+    	//FXMLLoader loader = new FXMLLoader();
+    	//String fxmlDocPath = "FxApp.fxml";
+    	//FileInputStream fxmlStream = new FileInputStream(fxmlDocPath);
+
+
+
+        primaryStage.setTitle("Treningsapp");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label userName = new Label("User Name:");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent e) {
+                actiontarget.setFill(Color.FIREBRICK);
+                actiontarget.setText("Something should happen");
+                primaryStage.setScene(scene2);
+                
+            }
+        });
+        
+        
+      //Scene 2
+        Label label2= new Label("This is the second scene");
+        label2.relocate(600, 400);
+        Group layout2= new Group();
+        layout2.getChildren().addAll(label2);
+        scene2= new Scene(layout2, 1200, 800);
+       
+
+        Scene loginScene = new Scene(grid, 1200, 800);
+        primaryStage.setScene(loginScene);
+        primaryStage.show();
     }
     
-    //something
 
+            
+    
     public static void main(String[] args) {
         launch(args);
     }
 }
+
