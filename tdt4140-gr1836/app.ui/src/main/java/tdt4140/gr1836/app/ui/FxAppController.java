0f1969292d4faccfb0b38dbbf40e2ffbb6d4e4fe
unknown <erling_sm@hotmail.com>
2018-02-14 12:48:25 +0100

    default project

--- tdt4140-gr1836/app.ui/src/main/java/tdt4140/gr1836/app/ui/FxAppController.java
+++ tdt4140-gr1836/app.ui/src/main/java/tdt4140/gr1836/app/ui/FxAppController.java
@@ -1,4 +1,15 @@
 package tdt4140.gr1836.app.ui;
 
-public class FxAppController {
+
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FxAppController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("loaded");
+    }
+
 }
