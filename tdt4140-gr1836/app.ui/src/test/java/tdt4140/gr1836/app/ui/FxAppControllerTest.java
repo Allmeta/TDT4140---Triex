948182d519a7ddc8ce50cc78ed7eb6cb1672d213
skotn <31900309+skotn@users.noreply.github.com>
2018-02-28 10:46:59 +0100

    added some functionality to new workout, tester commit på nytt

---
+++ tdt4140-gr1836/app.ui/src/test/java/tdt4140/gr1836/app/ui/FxAppControllerTest.java
@@ -0,0 +1,11 @@
+package tdt4140.gr1836.app.ui;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+
+public class FxAppControllerTest extends ApplicationTest {
+	
+
+}
