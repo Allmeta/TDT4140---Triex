5806016a4387bce2acbcdf9d34c273486bf73c0b
skotn <31900309+skotn@users.noreply.github.com>
2018-02-28 11:01:04 +0100

    Fjernet test for fxapp

--- tdt4140-gr1836/app.ui/src/test/java/tdt4140/gr1836/app/ui/FxAppTest.java
+++ tdt4140-gr1836/app.ui/src/test/java/tdt4140/gr1836/app/ui/FxAppTest.java
@@ -17,13 +17,6 @@ public class FxAppTest extends ApplicationTest {
 		GitlabCISupport.headless();
 	}
 	
-	@Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
 
     @Test
     public void testFxApp() {
