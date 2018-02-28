51c7880bf0e923640680e4fbf9ba7ceba91383bb
martehoffhagen <martehoffhagen@10.0.0.87>
2018-02-28 17:29:04 +0100

    Changed Cardio&StrengthWorkoutController

--- tdt4140-gr1836/app.ui/src/main/java/tdt4140/gr1836/app/ui/StrengthWorkoutController.java
+++ tdt4140-gr1836/app.ui/src/main/java/tdt4140/gr1836/app/ui/StrengthWorkoutController.java
@@ -2,34 +2,39 @@ package tdt4140.gr1836.app.ui;
 
 import java.awt.TextField;
 import java.io.IOException;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class StrengthWorkoutController {
 	
 	public static void main(String[] args) throws Exception {
-		        Calendar cal = Calendar.getInstance();
-		        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yy HH:mm a");
-		        File file = new File("CardioWorkout.txt"); 
-		        if (!file.exists() || file.isDirectory()) {
-		            PrintWriter writer = new PrintWriter("CardioWorkout.txt", "UTF-8");
-		            writer.close();
-		        }
-		        
-		        final PrintWriter output = new PrintWriter(new FileWriter(file, true));
-		        output.println("\n" + sdf.format(cal.getTime())); 
-		        JFrame frame = new CardioWorkoutController();
-		        frame.setTitle("Cardio Workout");
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yy HH:mm a");
+		File file = new File("CardioWorkout.txt");
+		if (!file.exists() || file.isDirectory()) {
+			PrintWriter writer = new PrintWriter("CardioWorkout.txt", "UTF-8");
+			writer.close();
+			}
+		
+		final PrintWriter output = new PrintWriter(new FileWriter(file, true));
+		output.println("\n" + sdf.format(cal.getTime()));
+		JFrame frame = new CardioWorkoutController();
+		frame.setTitle("Cardio Workout");
 		
 		output.close();
 		System.exit(0);
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+		frame.setVisible(true); 
+		}
 	
 	@FXML
 	public Button SubmitStrengthWorkout;
@@ -54,8 +59,6 @@ public class StrengthWorkoutController {
 	
 	@FXML
 	public TextField Information;
-
-	
 	
 	public void SubmitStrengthWorkout() throws IOException {
 		FxApp.showScene("MainItems.fxml");
@@ -65,8 +68,7 @@ public class StrengthWorkoutController {
 		FxApp.showScene("MainItems.fxml");
 	}
 	
-
-	}
+}
 		
 
 
