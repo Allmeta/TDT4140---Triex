df47a0bdabaeca30987feb730d34035a677ee329
gisleste <gisleste@gmail.com>
2018-04-08 10:19:42 +0200

    Comment

--- tdt4140-gr1836/app.ui/src/main/java/tdt4140/gr1836/app/ui/AlertHelper.java
+++ tdt4140-gr1836/app.ui/src/main/java/tdt4140/gr1836/app/ui/AlertHelper.java
@@ -4,7 +4,7 @@ package tdt4140.gr1836.app.ui;
 
 import javafx.scene.control.Alert;
 import javafx.stage.Window;
-
+/*hjelpeklasse for pop-up vindu ved registeringstilbakemelding*/
 public class AlertHelper {
 
     public static void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
